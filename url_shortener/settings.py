8e92d614849bdb9a4be468bfd508348c6f27764a
avaakash <as86414@gmail.com>
2021-04-19 03:59:12 +0530

    fixed issue with api authentication;

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -141,8 +141,8 @@ LOGIN_REDIRECT_URL = '/accounts/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
 
